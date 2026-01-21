5c7ed38c7195061979342fb1b07f4ff47c650c33
Markus Evanger <markus@kult.design>
2026-01-21 10:18:56 +0100

    fix next/sanity warnings

--- apps/frontend/src/server/sanity/sanity-auth-client.ts
+++ apps/frontend/src/server/sanity/sanity-auth-client.ts
@@ -5,5 +5,6 @@ export const authClient = createClient({
   projectId: env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: env.NEXT_PUBLIC_SANITY_API_VERSION,
+  useCdn: false,
   token: env.SANITY_API_READ_TOKEN,
 });
